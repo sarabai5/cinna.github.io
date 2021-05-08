5530b2468ca189e1a8a1bb1465af40f75a02c1aa
sarabai5 <62546274+sarabai5@users.noreply.github.com>
2021-05-07 23:20:17 -0400

    copy dist to root

---
+++ precache-manifest.6625f413111b1971bf40de6fc6a924d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a9b20c59fc70bb417d3316173eb2ad",
+    "url": "/assets/icons/media_email.png"
+  },
+  {
+    "revision": "99f84b87b8d776fced301e865c4ebc3e",
+    "url": "/assets/icons/media_ins.png"
+  },
+  {
+    "revision": "c8c53ad5d3433d9ffff31fb7eb924c06",
+    "url": "/assets/icons/media_linkedin.png"
+  },
+  {
+    "revision": "850d7a42e594e16f16a5365bc6e09261",
+    "url": "/assets/icons/resume_email.png"
+  },
+  {
+    "revision": "bcda7f693cb9dbe313659c262721bc65",
+    "url": "/assets/works/covers/cover1.png"
+  },
+  {
+    "revision": "f72ff33c624d8117eed5a2f39614d2ac",
+    "url": "/assets/works/covers/cover2.gif"
+  },
+  {
+    "revision": "dcfc8cf0ebb9b98ce611",
+    "url": "/css/app.2c24189e.css"
+  },
+  {
+    "revision": "27e06f8acc471faedcd278f567071fe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcfc8cf0ebb9b98ce611",
+    "url": "/js/app.5a8c7918.js"
+  },
+  {
+    "revision": "08f27f1d2dfd4f4ac095",
+    "url": "/js/chunk-vendors.790af666.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
